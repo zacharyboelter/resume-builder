181d30a9e87821b7f584ef832eb01e49e8f8be1f
Zachary Boelter <zboelter@gmail.com>
2023-02-23 16:10:01 -0800

    get and listen on PORT

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ const PORT = 4000
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(cors())
+
+app.get('/api', (req, res) => {
+    res.json({
+        message: 'Hello world',
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+})
