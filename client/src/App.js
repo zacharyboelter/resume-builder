3c04692384b76c8548d20cbca33f11d482a27cd6
Zachary Boelter <zboelter@gmail.com>
2023-02-25 14:48:33 -0800

    done for the day, work on resume portion tomorrow

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
-import Resume from './Resume'
+import Home from './components/Home'
+import Resume from './components/Resume'
 
 function App() {
+  const [result, setResult] = useState({});
+
   return (
 
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/resume' element={<Resume />}></Route>
+          <Route path='/' element={<Home setResult={setResult} />}></Route>
+          <Route path='/resume' element={<Resume result={result} />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
