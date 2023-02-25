27625c7fdc4d542bb5e7e7e046123e3993cafc0c
Zachary Boelter <zboelter@gmail.com>
2023-02-25 13:52:46 -0800

    error page component

--- client/src/ErrorPage.js
+++ client/src/ErrorPage.js
@@ -1,1 +1,18 @@
 //shown when an error occurs
+
+import React from "react";
+import { Link } from 'react-router-dom'
+
+
+const ErrorPage = () => {
+    return(
+        <div className="app">
+            <h3>
+                You have not provided your details. Head back to the {" "}
+                <Link to='/'>homepage</Link>
+            </h3>
+        </div>
+    )
+}
+
+export default ErrorPage
