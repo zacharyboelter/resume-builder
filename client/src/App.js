cca0a5c183da962a1e8e7c28011eb2b360c6a09c
Zachary Boelter <zboelter@gmail.com>
2023-02-25 13:33:02 -0800

    routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import Resume from './Resume'
 
 function App() {
   return (
+
     <div>
-      Hello World
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/resume' element={<Resume />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
