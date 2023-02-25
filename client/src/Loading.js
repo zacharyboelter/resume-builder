bead993dd8de20d321b33c92f1a32cc3d297eb24
Zachary Boelter <zboelter@gmail.com>
2023-02-25 13:48:57 -0800

    loading component

--- client/src/Loading.js
+++ client/src/Loading.js
@@ -1,1 +1,13 @@
 //contains the component shown to the user when the request is pending
+
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className="app">
+            <h1>Loading, please wait...</h1>
+        </div>
+    )
+}
+
+export default Loading
