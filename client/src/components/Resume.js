9d3bbaa7b45921323e290d8fc3703ca03cb1b037
Zachary Boelter <zboelter@gmail.com>
2023-02-27 16:49:31 -0800

    resume boilerplate and handlePrint function started

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,1 +1,23 @@
 // displays the AI-generated resume to the user
+
+import React from "react"
+import ErrorPage from './ErrorPage'
+
+
+const Resume = ({ result }) => {
+    if (JSON.stringify(result) === '{}') {
+        return <ErrorPage />
+    }
+
+    const handlePrint = () => alert('Print Successful.')
+    return (
+        <div>
+            <button onClick={handlePrint}>Print Page</button>
+            <main className="conatiner">
+                <p>Howdy!</p>
+            </main>
+        </div>
+    )
+}
+
+export default Resume
